b6a641151eb1b5a4aeb935ec4f57ffe2bbc692fe
Miguel Ramos <return.infox@gmail.com>
2026-01-15 20:06:28 -0300

    codigo actualizado del proyecto de jquery

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// array para marcar las tareas con prioridad
+let tareasPrioridad = [];
+
+// funcion para mostrar las notificaciones 
+function mostrarNotificacion(mensaje, tipo) {
+
+}
+
+// funcion para actualizar el contador de tareas en el navbar
+function actualizarContadorTareas() {
+
+}
+
+// funcion para marcar/desmarcar tareas con prioridad
+// toggle
+function togglePrioridad(tareaId) {
+
+}
+
+
+// function inicializar los eventos de jquery
+
+function inicializarEventos() {
+  $('.btn-prioridad').on('click', function() {
+    console.log('Botón de prioridad clickeado');
+  });
+}
